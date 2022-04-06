3446ef33161edb0fa872e0d4a3f7ed97b7d1d1c9
l1lypad <101284973+l1lypad@users.noreply.github.com>
2022-04-06 18:14:46 -0500

    Add solution to coding challenge from 4-6-2022

--- js/week1solutions.js
+++ js/week1solutions.js
@@ -10,7 +10,8 @@ function even_or_odd(number) {
     }
   }
 
-  
+
+
 //      RETURN NEGATIVE
 
 //   In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
@@ -33,6 +34,7 @@ function even_or_odd(number) {
   }
 
 
+
 //    BASIC MATHEMATICAL OPERATIONS
 
 // Your task is to create a function that does four basic mathematical operations.
@@ -50,4 +52,19 @@ function even_or_odd(number) {
     }else if(operation === '/'){
       return(value1/value2)
     }
+  }
+
+  //       SQUARE(N) SUM
+
+
+//   Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+//   For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+  function squareSum(numbers){
+    let sum = 0
+    for (let i = 0; i < numbers.length; i++){
+      sum += numbers[i] ** 2
+    }
+    return sum
   }
