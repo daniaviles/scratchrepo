470e0fb5e4f086efcc6714040861da7c9475c027
l1lypad <101284973+l1lypad@users.noreply.github.com>
2022-04-07 18:17:51 -0500

    Add solution to coding challenge from 4-7-2022

--- js/week1solutions.js
+++ js/week1solutions.js
@@ -68,3 +68,18 @@ function even_or_odd(number) {
     }
     return sum
   }
+  
+
+  //    Beginner Series #4 Cockroach
+
+//   The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+// For example:
+// 1.08 --> 30
+// Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+  function cockroachSpeed(s){
+    s = Number(Math.floor(s * 27.778))
+    return s
+    }
+    
