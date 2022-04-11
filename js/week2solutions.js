63b93a3b4936f9cc6a4c6adc071d61cb6e11be1c
l1lypad <101284973+l1lypad@users.noreply.github.com>
2022-04-11 18:28:37 -0500

    Add soultion from coding challenge 4-11-2022

--- js/week2solutions.js
+++ js/week2solutions.js
@@ -10,3 +10,13 @@ function checkForFactor (base, factor) {
         return false
     }
   }
+
+
+  
+//      Convert a Number to a String!
+// We need a function that can transform a number into a string.
+// What ways of achieving this do you know?
+
+  function numberToString(num) {
+    return num.toString();// Return a string of the number here!
+  }
